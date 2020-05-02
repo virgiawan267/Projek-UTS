31d66399b1ff03132f7bbeb59976fbb841bd7af5
virgiawan267 <virgiawanputra267@gmail.com>
2020-05-03 02:30:45 +0700

    berhasil hapus data montir

--- controller.js
+++ controller.js
@@ -227,3 +227,16 @@ exports.ubahlevel = function (req, res) {
             }
         });
 }; 
+
+//menghapus data montir
+exports.hapusMontir = function(req, res){
+    var id = req.body.id_montir;
+    connection.query('DELETE FROM t_montir WHERE id_montir=?', [id],
+    function (error, rows, fields) {
+        if (error) {
+            console.log(error);
+        } else {
+            response.ok("Berhasil Hapus Data", res)
+        }
+    });
+};
--- routes.js
+++ routes.js
@@ -45,5 +45,6 @@ module.exports = function(app) {
      app.route('/ubahservice')
      .put(jsonku.ubahservice);
 
-     
+     app.route('/hapusmontir')
+     .delete(jsonku.hapusMontir);
 }
